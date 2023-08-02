5deebe790fec72ad342b022b4506ddf144de6219
Brandon Bang <brandoncharlesbang@gmail.com>
2023-08-02 13:53:01 -0400

    Install Tailwind CSS

--- static/src/js/App.tsx
+++ static/src/js/App.tsx
@@ -4,14 +4,22 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello, World!</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="max-w-screen-sm mx-auto px-6 py-4">
+      <h1 className="text-3xl font-bold">Hello, World!</h1>
+
+      <div className="mt-8">
+        <button
+          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Increment
         </button>
+
+        <span className="ml-4">
+          Count is {count}
+        </span>
       </div>
-    </>
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './templates/app.html',
+    './static/src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
